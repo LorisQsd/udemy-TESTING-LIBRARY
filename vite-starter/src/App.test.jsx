ceb6bc58a0e28391ae3993d699e770b2562c5b07
Loris QUESADO <loris.quesado@oclock.school>
2024-09-01 18:28:40 +0200

    get by role instead of test

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -5,6 +5,8 @@ test("App contains correct heading", () => {
   // The method render a simulated DOM for whatever JSX we give it as an argument
   render(<App />);
   // We can access to the created simulated DOM by using the 'screen' global
-  const headingElement = screen.getByText(/learn react/i); // Regexp: (i): Case insensitive
+  // It is top priority to "select" your element with a role.
+  // That means your code is accessible and can be interprated from a screen reader
+  const headingElement = screen.getByRole("heading", { name: /learn react/i }); // Regexp: (i): Case insensitive
   expect(headingElement).toBeInTheDocument();
 });
