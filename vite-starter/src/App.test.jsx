adcd52fa36997e4060f39ca365542a473144ba33
Loris QUESADO <loris.quesado@oclock.school>
2024-09-01 17:41:32 +0200

    first lesson

--- vite-starter/src/App.test.jsx
+++ vite-starter/src/App.test.jsx
@@ -2,7 +2,9 @@ import { render, screen } from "@testing-library/react";
 import App from "./App";
 
 test("App contains correct heading", () => {
+  // The method render a simulated DOM for whatever JSX we give it as an argument
   render(<App />);
-  const headingElement = screen.getByText(/learn react/i);
+  // We can access to the created simulated DOM by using the 'screen' global
+  const headingElement = screen.getByText(/learn react/i); // Regexp: (i): Case insensitive
   expect(headingElement).toBeInTheDocument();
 });
