0dbdab5a7a99bc2b8ef2da846a93b4c7ef0fe5ad
Loris QUESADO <loris.quesado@oclock.school>
2024-09-04 20:36:16 +0200

    add popover TU and UI

--- sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
+++ sundae-starter/src/pages/summary/tests/SummaryForm.test.jsx
@@ -70,9 +70,25 @@ describe("Summary Form", () => {
   }); */
 
   test("popover responds to hover", async () => {
-    // const user = userEvent.setup();
+    const user = userEvent.setup();
+    render(<SummaryForm />);
+
     // popover start out hidden
+    const nullPopover = screen.queryByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(nullPopover).not.toBeInTheDocument();
+
     // popover appears on mouseover of checkbox label
+    const termsAndConditions = screen.getByText(/terms and conditions/i);
+    await user.hover(termsAndConditions);
+    const popover = screen.getByText(
+      /no ice cream will actually be delivered/i
+    );
+    expect(popover).toBeInTheDocument();
+
     // popover disappear when we mouse out
+    await user.unhover(termsAndConditions);
+    expect(popover).not.toBeInTheDocument();
   });
 });
