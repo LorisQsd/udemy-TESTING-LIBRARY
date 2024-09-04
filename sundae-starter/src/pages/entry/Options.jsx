7f7f8e3ec06f276853ef8309dca2565ad911e82a
Loris QUESADO <loris.quesado@oclock.school>
2024-09-04 21:15:35 +0200

    first asynchronous test

--- sundae-starter/src/pages/entry/Options.jsx
+++ sundae-starter/src/pages/entry/Options.jsx
@@ -1,3 +1,31 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import Row from "react-bootstrap/Row";
+import ScoopOption from "./ScoopOption";
+
 export default function Options({ optionType }) {
-  return <div />;
+  const [items, setItems] = useState([]);
+
+  // optionType is 'scoops' or 'toppings'
+  useEffect(() => {
+    axios
+      .get(`http://localhost:3030/${optionType}`)
+      .then((response) => setItems(response.data))
+      .catch(() => {
+        // TODO: handle error repsonse
+      });
+  }, [optionType]);
+
+  // TODO: replace `null` with ToppingOption when available
+  const ItemComponent = optionType === "scoops" ? ScoopOption : null;
+
+  const optionItems = items.map(({ imageName, imagePath }) => (
+    <ItemComponent
+      key={imageName}
+      imageName={imageName}
+      imagePath={imagePath}
+    />
+  ));
+
+  return <Row>{optionItems}</Row>;
 }
--- sundae-starter/src/pages/entry/tests/Options.test.jsx
+++ sundae-starter/src/pages/entry/tests/Options.test.jsx
@@ -3,11 +3,11 @@ import { expect } from "vitest";
 
 import Options from "../Options";
 
-test("displays image for each scoop option from server", () => {
+test("displays image for each scoop option from server", async () => {
   render(<Options optionType="scoops" />);
 
   // find images
-  const scoopImages = screen.getByAllRole("img", { name: /scoop$/i });
+  const scoopImages = await screen.findAllByRole("img", { name: /scoop$/i });
   expect(scoopImages).toHaveLength(2);
 
   // confirm alt text of images
